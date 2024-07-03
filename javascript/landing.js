9cdd08dd6e2f4bc3ecd1e70528d1127b93503941
Naema <94893024+ranmori@users.noreply.github.com>
2024-07-03 09:39:24 -0700

    Update landing.js

--- javascript/landing.js
+++ javascript/landing.js
@@ -90,6 +90,42 @@ function handleSignupFormSubmit(e) {
 }
 });
 
+
+// random
+console.clear();
+
+gsap.registerPlugin(ScrollTrigger);
+
+window.addEventListener("load", () => {
+  gsap
+    .timeline({
+      scrollTrigger: {
+        trigger: ".wrapper",
+        start: "top top",
+        end: "+=150%",
+        pin: true,
+        scrub: true,
+        markers: true
+      }
+    })
+    .to("img", {
+      scale: 2,
+      z: 350,
+      transformOrigin: "center center",
+      ease: "power1.inOut"
+    })
+    .to(
+      ".section.hero",
+      {
+        scale: 1.1,
+        transformOrigin: "center center",
+        ease: "power1.inOut"
+      },
+      "<"
+    );
+});
+
+
 // loader
 // function myFunction() {
 //     var x = document.getElementById("loader");
